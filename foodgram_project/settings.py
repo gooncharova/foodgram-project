24c328bb93aff8314115330ab3a84c63057644c5
Ольга <oe.goncharova@gmail.com>
2021-01-22 17:28:50 +0300

    fixed tiny bugs: flatpages, recipe_form

--- foodgram_project/settings.py
+++ foodgram_project/settings.py
@@ -94,13 +94,9 @@ WSGI_APPLICATION = 'foodgram_project.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
